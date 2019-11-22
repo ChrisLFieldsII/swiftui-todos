4018e97166f57a8bf14b0f6a3cd88728ef5e37d6
Christopher Fields <chrisfields@coreconstruction.com>
2019-11-21 20:07:22 -0600

    Initial Commit

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Christopher Fields on 11/21/19.
+//  Copyright © 2019 Christopher Fields. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
